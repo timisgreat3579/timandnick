81f9d61cbb680c1b72aa52c71c49ad64b058cf01
nick-tkachov <34717219+nick-tkachov@users.noreply.github.com>
2017-12-20 12:12:33 -0500

    Update loginDatabase.py

--- loginDatabase.py
+++ loginDatabase.py
@@ -7,9 +7,7 @@ if not found:
 import boto3
 
 session = boto3.resource('dynamodb',
-    aws_access_key_id='AKIAIOPUXE2QS7QN2MMQ',
-    aws_secret_access_key='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',
-    region_name="ca-central-1"
-)
+   
+
 table = session.Table('people')
 print(table.scan('peopleid'))
